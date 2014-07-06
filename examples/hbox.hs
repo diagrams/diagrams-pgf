28303576e69e8d62b10ddb2af21e62a0144ffe96
Christopher Chalmers <c.chalmers@me.com>
2014-07-06 11:37:46 +0100

    Added hbox example.

---
+++ examples/hbox.hs
@@ -0,0 +1,72 @@
+import Diagrams.Backend.PGF
+import Diagrams.Backend.PGF.CmdLine
+import Diagrams.Prelude
+import Diagrams.TwoD.Vector         (perp)
+
+import Data.Default
+
+
+-- The simplest way to construct a hbox with an envelope is to use
+--
+-- envelopedText = surfHbox mysurf "text" :: Diagram PGF R2
+--
+-- which can be used just like any other diagram. However, each box like this
+-- makes a call to TeX, so for multiple boxes this can become slow.
+--
+-- This examples shows how to use the OnlineTeX monad to construct a diagram
+-- with multiple hboxs with a single call to TeX.
+
+main = main1
+
+-- | The simplest option. The command line gives the option to use the default
+--   latex, context or plain tex surface (latex is the default).
+main1 = onlineMain example
+
+-- | Similar to 'onlineMain' but uses the specified surface.
+main2 = onlineMainWithSurf (def & command .~ "lualatex") example
+
+-- | Alternatively, 'renderOnlinePDF' can be used to directly generate a pdf.
+main3 = renderOnlinePGF "hbox.pdf" Absolute latexSurface example
+
+-- | "renderOnlinePDF'" allows choosing rendering options
+main4 = renderOnlinePGF' "hbox.tex"
+                         (def & sizeSpec .~ Width 300 & standalone .~ True)
+                         example
+
+example :: OnlineTeX (Diagram PGF R2)
+example = frame 5 . scale 10 <$> hboxLines "\\TeX"
+
+-- Use the envelope from the hbox to label the width, height and depth.
+hboxLines :: String -> OnlineTeX (Diagram PGF R2)
+hboxLines str = do
+  txt <- onlineHbox str
+
+  let h = envelopeV unitY txt
+      d = envelopeV unit_Y txt
+      w = envelopeV unitX txt
+
+  hArrow <- labeledArrow False "height" h
+  dArrow <- labeledArrow True "depth" d
+  wArrow <- labeledArrow False "width" w
+
+  return $ (txt <> boundingRect txt <> fromOffsets [w])
+           ||| strutX 1 ||| (hArrow === dArrow)
+           === strutY 1
+           === wArrow
+
+-- Draw an arrow with a horizontal label above or below.
+labeledArrow :: Bool -> String -> R2 -> OnlineTeX (Diagram PGF R2)
+labeledArrow above label r = diaArrow <$> onlineHbox label
+  where
+    diaArrow txt = atAngle ((direction . rev . perp) r)
+                                 (arr # translate (negateV $ r^/2))
+                                 (txt # centerXY # scale 0.1 # frame 0.3)
+                             # translate (r^/2)
+      where
+        rev = if above then id else negateV
+        arr = arrowV' ops r
+        ops = def & arrowHead .~ spike
+                  & arrowTail .~ spike'
+                  & lengths   %~ (^/2)
+
+
